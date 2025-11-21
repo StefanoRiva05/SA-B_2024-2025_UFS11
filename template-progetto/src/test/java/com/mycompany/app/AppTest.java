48268a3357eb2319f78f31d04008240ce4328280
StefanoRiva05 <stefano.riva@itsrizzoli.it>
2025-11-21 17:27:51 +0100

    Aggiunta di test per i dipendenti

--- template-progetto/src/test/java/com/mycompany/app/AppTest.java
+++ template-progetto/src/test/java/com/mycompany/app/AppTest.java
@@ -1,5 +1,6 @@
 package com.mycompany.app;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,6 +10,15 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( false );
+        assertTrue( true );
     }
+
+    @Test
+    public void testaDipendente(){
+        Dipendente d1 = new Dipendente(Dipendente.ruoli.Frontend, "Luca", "Rossi", 2500);
+        assertEquals("Luca", d1.getNome());
+        d1.setNome("Giorgio");
+        assertEquals("Giorgio", d1.getNome());
+    }
+
 }
